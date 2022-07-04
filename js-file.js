50d39d2a3c8a47517db04c1d5f53f07b0954e9b3
Tommy <tommy128works@gmail.com>
2022-07-04 19:55:57 -0400

    Hover over tiles to create trail

--- js-file.js
+++ js-file.js
@@ -2,21 +2,27 @@ const container = document.querySelector('#container');
 
 for (let row = 0; row < 16; row++) {
   newRow = document.createElement('div');
-  newRow.setAttribute('id', 'row');
   newRow.style.cssText = 'display: flex; justify-items: center;'
   
   for (let col = 0; col < 16; col++) {
     newCol = document.createElement('div');
     newCol.textContent = col + 1;
     newCol.style.cssText = 'text-align: center; width: 50px; height: 50px;'
+    newCol.classList.add('tile');
+    // newCol.addEventListener('mouseover', () => {
+    //   newCol.classList.add('trail');
+    // })
     newRow.appendChild(newCol);
   }
-  
   container.appendChild(newRow);
 }
 
+const grid = document.querySelector('#container');
+
+grid.addEventListener('mouseover', (event) => {
+  event.target.classList.add('trail');
+})
 
 
 
-// need to put in css style code for flex
-// need to set width and height in ratio (to make square)
+
