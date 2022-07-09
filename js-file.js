be1da93f0780084c5a925798690ae5b2b14a444c
Tommy <tommy128works@gmail.com>
2022-07-09 10:42:38 -0400

    Add user prompt for grid size (without functionality to change grid)

--- js-file.js
+++ js-file.js
@@ -9,9 +9,6 @@ for (let row = 0; row < 16; row++) {
     newCol.textContent = col + 1;
     newCol.style.cssText = 'text-align: center; width: 50px; height: 50px;'
     newCol.classList.add('tile');
-    // newCol.addEventListener('mouseover', () => {
-    //   newCol.classList.add('trail');
-    // })
     newRow.appendChild(newCol);
   }
   container.appendChild(newRow);
@@ -23,6 +20,17 @@ grid.addEventListener('mouseover', (event) => {
   event.target.classList.add('trail');
 })
 
+const button = document.querySelector('button');
+let gridSize = 0;
+button.addEventListener('click', () => {
+  
+  do {
+    gridSize = prompt("Select a grid size between 1-100");
+  }
+  while (gridSize < 1 || gridSize > 100 || isNaN(gridSize))
+
+  
+})
 
 
 
